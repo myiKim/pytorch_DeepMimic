2f8bede0c0966631c2e4b8f55236ac0f739889c1
MyiKim <102040823+myiKim@users.noreply.github.com>
2023-12-10 19:19:35 +0900

    add setup file
    for completeness

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="heygogoMyi",
+    version='0.0.1',
+    install_requires=['gym',
+                      'pybullet',
+                      'numpy',
+                      'matplotlib',
+                      'scipy']
+)
